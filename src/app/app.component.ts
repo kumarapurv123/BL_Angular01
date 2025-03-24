53378fdfbbfcb3522cc2902f8e2a7791af64feed
kumarapurv123 <91779441+kumarapurv123@users.noreply.github.com>
2025-03-24 11:49:31 +0530

    UC5 Added Error Handling on Name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports:[FormsModule],
+  imports:[FormsModule,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -17,4 +18,10 @@ export class AppComponent {
   }
   
   userName = '';
+  errorMessage = '';
+
+  validateName() {
+    const isValid = /^[A-Z][a-z]{2,}$/.test(this.userName);
+    this.errorMessage = isValid ? '' : 'Name must start with a capital letter and be at least 3 letters long.';
+  }
 }
