4ec05d142581ae26266098d4888a8b0d6c22c34b
kumarapurv123 <91779441+kumarapurv123@users.noreply.github.com>
2025-03-24 11:42:40 +0530

    UC3 Added URL & Event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Hello from BridgeLabz';
   logoPath = './bl_logo.png';
+
+  bridgeLabzURL = 'https://www.bridgelabz.com/'; 
+  openBridgeLabz() {
+    window.open(this.bridgeLabzURL, '_blank');
+  }
   
 }
